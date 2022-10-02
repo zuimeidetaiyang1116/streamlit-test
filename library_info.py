6b954b3bdc82a176b582372aac1688c6e635dfc7
zuimeidetaiyang1116 <61719611+zuimeidetaiyang1116@users.noreply.github.com>
2022-10-02 15:14:27 +0800

    更改获取token的方式
    获取streamlit的token

--- library_info.py
+++ library_info.py
@@ -22,8 +22,7 @@ next_date = (today + offset).strftime('%Y-%m-%d')
 order_time_list = ['早上', '早上2', '中午', '下午', '下午2', '晚上', '晚上2']
 date_list = [date, next_date]
 
-with open("token.txt", "r", encoding='utf-8')as f:
-    token = f.read()
+token = ""
 
 cookie = f"HWWAFSESID=4f4ad7c4620d81af12; HWWAFSESTIME=1664095148908; sid=3; surl=jxut; SmartUserRole=; Auth-Token={token}"
 headers = {
