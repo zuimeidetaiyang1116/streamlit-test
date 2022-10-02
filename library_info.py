ab79fb9ca1e02f7b5b6068c08e7a2007ef5b4808
zuimeidetaiyang1116 <61719611+zuimeidetaiyang1116@users.noreply.github.com>
2022-10-02 17:03:36 +0800

    Update library_info.py

--- library_info.py
+++ library_info.py
@@ -38,7 +38,7 @@ headers = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.138 Safari/537.36 NetType/WIFI MicroMessenger/7.0.20.1781(0x6700143B) WindowsWechat(0x6307062c)',
     'Cookie': cookie,
 }
-
+st.write(cookie)
 
 def get_date_seats(date):
     data = {
