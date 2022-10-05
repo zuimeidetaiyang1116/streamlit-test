9fc213d3294eb726b94aa8af4b337b06eeb09522
zuimeidetaiyang1116 <61719611+zuimeidetaiyang1116@users.noreply.github.com>
2022-10-05 12:52:40 +0800

    Update library_info.py

--- library_info.py
+++ library_info.py
@@ -12,7 +12,6 @@ import random
 import streamlit as st
 
 import requests
-from send_message import send_msg
 
 msg = ""
 res = ""
@@ -24,16 +23,17 @@ next_date = (today + offset).strftime('%Y-%m-%d')
 order_time_list = ['早上', '早上2', '中午', '下午', '下午2', '晚上', '晚上2']
 date_list = [date, next_date]
 
-token = "3ff3b7a24f99f2ff99ba052cacf64375"
-while not st.button("爬取"):
-    token_value = st.text_input("请输入token")
-    st.write(token_value)
-    if token_value:
-        token = token_value
-        st.write("token",token)
-        break
+token = "c3e64a482b2f6894b92170dab36b4fc4"
+# token_value = st.text_input("请输入token")
+# token = token_value
+# while not st.button("爬取"):
+#     token_value = st.text_input("请输入token")
+#     if token_value:
+#         token = token_value
+
 
 cookie = f"HWWAFSESID=4f4ad7c4620d81af12; HWWAFSESTIME=1664095148908; sid=3; surl=jxut; SmartUserRole=; Auth-Token={token}"
+
 headers = {
     "Host": "jxut.educationgroup.cn"
     , "Connection": "keep-alive"
@@ -50,7 +50,7 @@ headers = {
     , "Accept-Encoding": "gzip, deflate"
     , "Accept-Language": "zh-CN,zh;q=0.9,en-US;q=0.8,en;q=0.7"
 }
-st.write(cookie)
+
 
 def get_date_seats(date):
     data = {
@@ -58,11 +58,14 @@ def get_date_seats(date):
     }
     url = "http://jxut.educationgroup.cn/tsg/kzwWx/getSjd"
     session = requests.session()
-    pprint.pprint(headers)
+    # pprint.pprint(headers)
     page_json = session.post(url=url, headers=headers, data=data).text
-    st.write(page_json)
     pprint.pprint(page_json)
-    time_data = json.loads(page_json)
+    try:
+        time_data = json.loads(page_json)
+    except:
+        print("没有爬取到有效数据，可能是cookie中的token过期了。")
+        return []
     timeid_data = {}
     for i in time_data['data']:
         sjdName = i['sjdName']
@@ -77,7 +80,7 @@ def get_seates(date, id):
     }
     url = "http://jxut.educationgroup.cn/tsg/kzwWx/getZws"
     page_json = requests.post(url=url, headers=headers, data=data).text
-    st.write(page_json)
+    # st.write(page_json)
     wait_data = json.loads(page_json)
     # pprint.pprint(wait_data)
     # with open("已预约.json", 'w', encoding='utf-8')as f:
@@ -86,7 +89,7 @@ def get_seates(date, id):
     zwid_data = {}
     for i in zwList:
         like_id = int(i['zwCode'])
-        if like_id > 459:
+        if like_id > 456:
             zwid_data[like_id] = i['id']
     return zwid_data
 
@@ -136,18 +139,34 @@ def taskdate(date, res):
                     res += f"\t\t可预约的座位号有：{id_list}\n"
                     print('图书馆1F_1楼渊明阁,>=460的座位有：', len(zwid_data))
                     pprint.pprint(zwid_data)
-                    result = order_seats(date, sdj_id, zwid_data[order_id])
-                    if result["code"] == 200:
-                        res += "\t\t预约的座位号：{order_id}\n"
+                    result_json = order_seats(date, sdj_id, zwid_data[order_id])
+                    result = json.loads(result_json)
+                    print("预约的座位号的请求：", result)
+                    if result["status"] == "success":
+                        print(f"\t\t{result['message']},座位号：{order_id}\n")
+                        res += f"\t\t{result['message']},座位号：{order_id}\n"
                 else:
                     res += "\t\t没有有效的座位\n"
             except:
                 pprint.pprint(timeid_data)
     return res
 
-for date in date_list:
-    msg += find_date_seats(date, '')
-    res += taskdate(date, res)
-send_msg("图书馆有效座位号> 459", msg, "txt")
-send_msg("预约座位", res, "txt")
+def do_task():
+    global msg
+    for date in date_list:
+        msg += find_date_seats(date, '')
+        # res += taskdate(date, res)
+    with open ("图书馆有效座位号.txt",'w',encoding="utf-8")as f:
+        f.write(msg)
+    # send_msg("图书馆有效座位号> 459", msg, "txt")
+    # send_msg("预约座位", res, "txt")
 
+if __name__ == '__main__':
+    st.header("hello")
+    Token = st.text_input("请输入token")
+    while True:
+        if token :
+            token = Token
+            do_task()
+            break
+    st.write("over")
